f17069d434604a08e5d245e9ca3bbb513cd72e89
Thakor Jenil Kamleshbhai <103255558+Jenil45@users.noreply.github.com>
2022-09-07 08:59:42 +0530

    Update StackUsiingLL.java

--- Data Structure/Stack/StackUsiingLL.java
+++ Data Structure/Stack/StackUsiingLL.java
@@ -11,3 +11,19 @@ class Node1
         next = null;
     }
 }
+
+public class StackUsiingLL {
+    public static void main(String[] args) {
+        StackLL s1 = new StackLL();
+        s1.push(45);
+        s1.push(93);
+        s1.push(18);
+        s1.push(33);
+
+        while (!s1.isEmpty())
+        {
+            System.out.print(s1.peek() + " ");
+            s1.pop();
+        }
+    }
+}
