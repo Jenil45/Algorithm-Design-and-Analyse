2f52f4da8037ded6a51c5e2e2d807642abcf3c86
Thakor Jenil Kamleshbhai <103255558+Jenil45@users.noreply.github.com>
2022-09-08 22:34:14 +0530

    Update StackUsingArrayList.java

--- Data Structure/Stack/StackUsingArrayList.java
+++ Data Structure/Stack/StackUsingArrayList.java
@@ -3,6 +3,48 @@ package com.gecg;
 import java.util.*;
 import java.util.ArrayList;
 
+class StackAL
+{
+    ArrayList<Integer> list = new ArrayList<>();
+
+    public boolean isEmpty()
+    {
+        if (list.size() == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void push(int data)
+    {
+        list.add(data);
+    }
+
+    public int pop()
+    {
+        if (isEmpty())
+        {
+            System.out.println("The stack is empty");
+            return -1;
+        }
+        int top = list.get(list.size()-1);
+        list.remove(list.size()-1);
+        return top;
+    }
+
+    public int peek()
+    {
+        if (isEmpty())
+        {
+            return -1;
+        }
+        return list.get(list.size()-1);
+    }
+}
 
 public class StackUsingArraylist {
     public static void main(String[] args) {
