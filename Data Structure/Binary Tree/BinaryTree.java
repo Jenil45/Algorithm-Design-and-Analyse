1432d1f5c572fe8f4ddadf8522984f8cf6a58882
Thakor Jenil Kamleshbhai <103255558+Jenil45@users.noreply.github.com>
2022-09-11 20:15:28 +0530

    Update BinaryTree.java

--- Data Structure/Binary Tree/BinaryTree.java
+++ Data Structure/Binary Tree/BinaryTree.java
@@ -124,6 +124,32 @@ public class BinaryTree {
         }
     }
   
+  // count of node
+    public static int countOfNode(Node root)
+    {
+        if (root==null)
+        {
+            return 0;
+        }
+        int leftNode = countOfNode(root.left);
+        int rightNode = countOfNode(root.right);
+        return leftNode + rightNode + 1;
+    }
+
+    // sum of node
+    public static int sumOfNodes(Node root)
+    {
+        if (root == null)
+        {
+            return 0;
+        }
+
+        int leftSum = sumOfNodes(root.left);
+        int rightSum = sumOfNodes(root.right);
+
+        return leftSum+rightSum+root.data;
+    }
+  
       public static void main(String[] args) {
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6 ,-1 ,-1};
         System.out.println("Binary Tree : ");
