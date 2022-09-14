3198b27ccfc039d3b8a9ee3e5cdf0828d08f0848
Thakor Jenil Kamleshbhai <103255558+Jenil45@users.noreply.github.com>
2022-09-14 21:52:16 +0530

    Create BinarySearchTress.java

---
+++ Data Structure/Binary Tree/Binary Search Tree/BinarySearchTress.java
@@ -0,0 +1,42 @@
+package com.gecg;
+import java.util.*;
+/*
+    we mostly use this trees in searching
+    Normally in binary tree if we search an element then it takes O(n) time
+    but in this binary search tree case it will be takes O(height of tree) time
+    Height of tree = log n        where n = no. of node
+
+    BST properties :
+    (i) It follows all properties of binary trees.
+    (ii) Left subtree Node < root.
+    (iii) Right subtree Node > root.
+    (iv) Left & Right subtrees are also BST with no duplicates.
+
+    Special properties :
+    Inorder traversal of BST gives us a sorted sequence
+
+    BST search :
+    We do binary search algorithm to search it (In actual we compare our key with the middle element
+    but here we compare  it with our root element
+
+    **Important :-
+    Most of the problem of BST are solved by recursion
+    i.e., by dividing the problem into sub problems  & making recursive calls on subtrees
+ */
+
+
+
+public class BinarySearchTress {
+    static class NodeBST
+    {
+        int data;
+        NodeBST left;
+        NodeBST right;
+
+        NodeBST(int data)
+        {
+            this.data = data;
+            left = null;
+            right = null;
+        }
+    }
